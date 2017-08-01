2435cf4a8739595693e8a85391b018f9e7725950
Larisasmiles <larisasmiles@gmail.com>
2017-08-01 09:28:11 -0600

    initial commit

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,27 @@
+# pantry = Pantry.new
+# # => <Pantry...>
+# # Checking and adding stock
+# pantry.stock
+# # => {}
+# pantry.stock_check("Cheese")
+# # => 0
+# pantry.restock("Cheese", 10)
+# pantry.stock_check("Cheese")
+# # => 10
+# pantry.restock("Cheese", 20)
+# pantry.stock_check("Cheese")
+# # => 30
 class Pantry
+    attr_reader :stock
+  def initialize
+    @stock = Hash.new(0)
+  end
 
+  def stock_check(item)
+    @stock[item]
+  end
+
+  def restock(item, amount)
+    @stock[item] += amount
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -1,7 +1,40 @@
 require './lib/pantry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 
 class PantryTest < Minitest::Test
 
+  def test_if_it_exists
+    p = Pantry.new
+
+    assert_instance_of Pantry, p
+  end
+
+  def test_if_it_returns_stock_as_a_hash
+    p = Pantry.new
+    p.stock
+
+    assert_equal ({}), p.stock
+  end
+
+  def test_if_it_stock_checks
+    p = Pantry.new
+    p.stock
+    p.stock_check("cheese")
+
+    assert_equal 0, p.stock_check("cheese")
+  end
+
+  def test_if_it_restocks
+    p = Pantry.new
+    p.stock
+    p.restock("cheese", 10)
+
+    assert_equal 10, p.stock_check("cheese")
+  end
+
+  def test
+
+  end
 end
